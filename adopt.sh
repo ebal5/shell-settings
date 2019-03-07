bdd0b568f31a09e4a089904fa44cb9e0849b2449
shuichi tani <eval.scheme@gmail.com>
2019-03-08 01:06:13 +0900

    settings for xmodmap

--- adopt.sh
+++ adopt.sh
@@ -4,10 +4,12 @@ zrcpos="$HOME/.zshrc"
 trcpos="$HOME/.tmux.conf"
 srcpos="$HOME/.config/shellrc"
 pystpos="$HOME/.config/pythonstartup.py"
+xmpos="$HOME/.Xmodmap"
 [[ -f $zrcpos ]] && mv $zrcpos ${zrcpos}.orig
 [[ -f $trcpos ]] && mv $trcpos ${trcpos}.orig
 [[ -f $srcpos ]] && mv $srcpos ${srcpos}.orig
 [[ -f $pystpos ]] && mv $pystpos ${pystpos}.orig
+[[ -f $xmpos ]] && mv $xmpos ${xmpos}.orig
 
 dir=$(cd $(dirname $0); pwd)
 ln -s $dir/.zshrc $zrcpos
@@ -15,3 +17,5 @@ ln -s $dir/.tmux.conf $trcpos
 ln -s $dir/shellrc $srcpos
 ln -s $dir/.latexmkrc $HOME/
 ln -s $dir/pythonstartup.py $pystpos
+ln -s $dir/.Xmodmap $xmpos
+
