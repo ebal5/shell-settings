95a3e57eb81d9ddbb06af8ea835e8d27edc1412a
Shuichi Tani <muminn4@gmail.com>
2019-04-16 18:18:42 +0900

    share the TeX files

--- adopt.sh
+++ adopt.sh
@@ -5,11 +5,13 @@ trcpos="$HOME/.tmux.conf"
 srcpos="$HOME/.config/shellrc"
 pystpos="$HOME/.config/pythonstartup.py"
 gitpos="$HOME/.gitconfig"
+texmf="$HOME/.texmf"
 [[ -f $zrcpos ]] && mv $zrcpos ${zrcpos}.orig
 [[ -f $trcpos ]] && mv $trcpos ${trcpos}.orig
 [[ -f $srcpos ]] && mv $srcpos ${srcpos}.orig
 [[ -f $pystpos ]] && mv $pystpos ${pystpos}.orig
 [[ -f $gitpos ]] && mv $gitpos ${gitpos}.orig
+[[ -d $texmf ]] && mv $texmf ${texmf}.orig
 
 dir=$(cd $(dirname $0); pwd)
 ln -s $dir/.zshrc $zrcpos
@@ -18,3 +20,4 @@ ln -s $dir/shellrc $srcpos
 ln -s $dir/.latexmkrc $HOME/
 ln -s $dir/pythonstartup.py $pystpos
 ln -s $dir/.gitconfig $gitpos
+ln -s $dir/.texmf $HOME/.texmf
