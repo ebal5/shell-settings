c423557d3eef0e0639dbe26b1141534e0fbcae64
Shuichi Tani <muminn4@gmail.com>
2019-10-22 13:25:13 +0900

    remove 'exit'

--- home/bin/xkeysnail.sh
+++ home/bin/xkeysnail.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 if pgrep xkeysnail > /dev/null; then
-    return 2>&- || exit
+    return 2>&-
 fi
 if [ ! -x /usr/bin/xkeysnail ]; then
-    return 2>&- || exit
+    return 2>&-
 fi
 exec >> /tmp/xkeysnail.log 2>&1
 xhost +SI:localuser:xkeysnail > /dev/null
